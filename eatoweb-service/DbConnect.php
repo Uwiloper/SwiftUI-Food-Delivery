602a4508034d58108e33f683fcb68acbe0da7792
uwiloper <132973171+Uwiloper@users.noreply.github.com>
2025-12-24 00:00:08 -0500

    Refactor database connection in DbConnect.php
    Updated database connection parameters to use placeholders.

--- eatoweb-service/DbConnect.php
+++ eatoweb-service/DbConnect.php
@@ -2,7 +2,7 @@
     //permitir acceso desde cualquier origen
     header("Access-Control-Allow-Origin: *");
 
-    $cn = new PDO("mysql:host=mysql-uwil.alwaysdata.net;dbname=uwil_eatoo", "uwil_1227", "Eatoo2025");
+    $cn = new PDO("mysql:host=mysql-uwil.alwaysdata.net;dbname={Database}", "{User}", "{Password}");
 
 
     //$cn = new PDO("mysql:host=localhost;dbname=eatoo_db", "root", "");
